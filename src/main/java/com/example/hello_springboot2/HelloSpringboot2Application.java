23de7275bfbb899a31ae10af09c99e92dfa6bc53
moho852 <moho191113@gmail.com>
2024-06-07 21:36:30 +0900

    dev member

---
+++ src/main/java/com/example/hello_springboot2/HelloSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.example.hello_springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringboot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hello_springboot2/HelloSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
